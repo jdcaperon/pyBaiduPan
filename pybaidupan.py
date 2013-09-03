8f66c078ab7165b3330435c243e9f685e7021dd2
katherlee <lijia1608@gmail.com>
2013-09-03 22:25:32 +0800

    Added download, upload, with multipart support. Fixed minor errors.

--- pybaidupan.py
+++ pybaidupan.py
@@ -8,7 +8,7 @@
 #    python-requests 1.2.3                        #
 ###################################################
 
-import requests
+import requests, hashlib, json
 
 KEY='Pfb12nmBr971bAaB33ubfXbp'
 SEC='4ihI1zVf1tk2tjOEVUBxvQa35UZAf3xq'
@@ -18,13 +18,13 @@ auth_url='https://openapi.baidu.com/oauth/2.0/device/code'
 token_url='https://openapi.baidu.com/oauth/2.0/token'
 quota_url='https://pcs.baidu.com/rest/2.0/pcs/quota'
 file_url='https://pcs.baidu.com/rest/2.0/pcs/file'
-cloud_url='https://psc.baidu.com/rest/2.0/pcs/service/cloud_dl'
+cloud_url='https://psc.baidu.com/rest/2.0/pcs/services/cloud_dl'
 
 def oauth_device(appkey, appsec):
     device_para={'client_id' : appkey, 'response_type': 'device_code', 'scope': 'basic,netdisk'}
     res=requests.post(auth_url, device_para)
     device_json=res.json()
-    #auth_err(device_json)
+    auth_err(device_json)
     print('''Follow these steps to complete authentication:
     1. Visit '''+ device_json['verification_url']+ ''' in your browser;
     2. Copy or input the following code when asked: '''+device_json['user_code']+'''
@@ -34,22 +34,22 @@ def oauth_device(appkey, appsec):
     token_para={'grant_type': 'device_token', 'code': device_json['device_code'], 'client_id': appkey, 'client_secret': appsec}
     res=requests.post(token_url, token_para)
     token_json=res.json()
-    #auth_err(token_json)
+    auth_err(token_json)
     return {'access_token': token_json['access_token'], 'refresh_token': token_json['refresh_token']}
 
 def oauth_refresh(appkey, appsec, refresh_token):
     para={'grant_type': 'refresh_token', 'refresh_token': refresh_token, 'client_id': appkey, 'client_secret': appsec}
     res=requests.post(token_url, para)
-    json=res.json()
-    #auth_err(json)
-    return {'access_token': json['access_token'], 'refresh_token': json['refresh_token']}
+    res_json=res.json()
+    auth_err(res_json)
+    return {'access_token': res_json['access_token'], 'refresh_token': res_json['refresh_token']}
 
 def get_quota(access_token):
     para={'method': 'info', 'access_token': access_token}
     res=requests.get(quota_url,params=para)
-    json=res.json()
-    #api_err(json)
-    return json
+    res_json=res.json()
+    api_err(res_json)
+    return res_json
 
 def list_file(access_token, r_path, by=None, order=None, limit=None):
     para={'access_token': access_token, 'method': 'list', 'path': r_path}
@@ -60,52 +60,53 @@ def list_file(access_token, r_path, by=None, order=None, limit=None):
     if limit:
         para['limit']=limit
     res=requests.get(file_url,params=para)
-    json=res.json()
-    #api_err(json)
-    return json
+    res_json=res.json()
+    api_err(res_json)
+    return res_json
 
 def make_dir(access_token, r_path):
-    para={'access_token': access_token, 'method': 'mkdir', 'path': path}
-    res=requests.post(file_url, para)
-    json=res.json()
-    #api_err(json)
-    return json
+    para={'access_token': access_token, 'method': 'mkdir', 'path': r_path}
+    res=requests.post(file_url, params=para)
+    res_json=res.json()
+    api_err(res_json)
+    return res_json
 
 def file_info(access_token, r_path):
-    para={'access_token': access_token, 'method': 'meta', 'path': path}
+    para={'access_token': access_token, 'method': 'meta', 'path': r_path}
     res=requests.get(file_url, params=para)
-    json=res.json()
-    #api_err(json)
-    return json
+    res_json=res.json()
+    api_err(res_json)
+    return res_json
 
 def move(access_token, from_path, to_path):
     para={'access_token': access_token, 'method': 'move', 'from': from_path, 'to': to_path}
-    res=requests.post(file_url, para)
-    json=res.json()
-    #api_err(json)
-    return json
+    res=requests.post(file_url, params=para)
+    res_json=res.json()
+    api_err(res_json)
+    return res_json
 
 def copy(access_token, from_path, to_path):
     para={'access_token': access_token, 'method': 'copy', 'from': from_path, 'to': to_path}
-    res=requests.post(file_url, para)
-    json=res.json()
-    #api_err(json)
-    return json
+    res=requests.post(file_url, params=para)
+    res_json=res.json()
+    api_err(res_json)
+    return res_json
 
 def delete(access_token, r_path):
     para={'access_token': access_token, 'method': 'delete', 'path': r_path}
-    json=res.json()
-    #api_err(json)
-    return json
+    res=requests.get(file_url, params=para)
+    res_json=res.json()
+    api_err(res_json)
+    return res_json
 
 def search(access_token, r_path, keyword, recursive=False):
     para={'access_token': access_token, 'method': 'search', 'path': r_path, 'wd': keyword}
     if recursive:
         para['re']='1'
     res=requests.get(file_url, params=para)
-    json=res.json()
-    #api_err(json)
-    return json
+    res_json=res.json()
+    api_err(res_json)
+    return res_json
 
 def cloud(access_token, save_path, source_url, expires=False, rate_limit=False, timeout=False, callback=False):
     para={'access_token': access_token, 'method': 'add_task', 'save_path': save_path, 'source_url': source_url}
@@ -117,8 +118,63 @@ def cloud(access_token, save_path, source_url, expires=False, rate_limit=False, 
         para['timeout']=timeout
     if callback:
         para['callback']=callback
-    res=requests.post(cloud_url, para)
-    json=res.json()
-    #api_err(json)
-    return json
+    res=requests.post(cloud_url, params=para)
+    res_json=res.json()
+    api_err(res_json)
+    return res_json
 
+def download(access_token, r_path, l_path, chunksize=None):
+    para={'access_token':access_token, 'method': 'download', 'path': r_path}
+    #To do: check if file exists
+    size=file_info(access_token, r_path)['list'][0]['size']
+    with open(l_path, 'wb') as f:
+        if chunksize:
+            pos = 0
+            print('0.0%', end='')
+            while pos <= size:
+                headers={'range':'bytes=' + str(pos) + '-' + str(pos+chunksize-1)}
+                res= requests.get('https://d.pcs.baidu.com/rest/2.0/pcs/file', params=para, headers=headers)
+                length=int(res.headers['Content-Length'])
+                if pos <= size - chunksize and length < chunksize:
+                    pass
+                api_err(res.json())
+                f.write(res.content)
+                print('\r{:.1f}%'.format((pos+length)/size*100), end='')
+                pos+= chunksize
+            print('\rDone')
+        else:
+            res=requests.get('https://d.pcs.baidu.com/rest/2.0/pcs/file', params=para)
+            f.write(res.content)
+
+def upload_single(access_token, r_path, l_path, ondup='overwrite'):
+    para={'access_token': access_token, 'path': r_path, 'method': 'upload', 'ondup': ondup}
+    with open(l_path, 'rb') as f:
+        res=requests.post('https://c.pcs.baidu.com/rest/2.0/pcs/file', params=para, files={'file':f})
+        res_json=res.json()
+        api_err(res_json)
+        return res_json
+
+def upload_multiple(access_token, r_path, l_path, chunksize=134217728, ondup='overwrite'):
+    md5_list=[]
+    with open(l_path, 'rb') as f:
+        while True:
+            chunk = f.read(chunksize)
+            para={'access_token': access_token, 'method': 'upload', 'type': 'tmpfile'}
+            res=requests.post('https://c.pcs.baidu.com/rest/2.0/pcs/file', params=para, files={'file':chunk})
+            res_json=res.json()
+            api_err(res_json)
+            md5_list.append(res_json['md5'])
+            if(len(chunk) < chunksize):
+                break
+    httpbody='param='+json.dumps({'block_list':md5_list})
+    para={'access_token':access_token, 'method': 'createsuperfile', 'path': r_path, 'ondup': ondup}
+    res=requests.post(file_url, params=para, data=httpbody.encode('utf-8'))
+    res_json=res.json()
+    api_err(res_json)
+    return res_json
+
+def api_err(res_json):
+    print(res_json)
+
+def auth_err(auth_json):
+    print(auth_json)
